f8420b5c552634dd1cf941b49048a81ad723e210
laszloh <laszloh517@gmail.com>
2022-04-24 10:18:22 +0200

    Day 1 - print, input, variables

---
+++ day-1_printing-to-console/band_name_generator.py
@@ -0,0 +1,8 @@
+print('Band name generator application')
+name = input('What is your name?\n')
+print(f'Hello {name}!')
+city = input('Which city did you grow up in?\n')
+pet = input('What is the name of your pet?\n')
+
+print(f'{name}! Your band name could be {city} {pet}.')
+
---
+++ day-1_printing-to-console/print.py
@@ -0,0 +1,10 @@
+print('Day 1 - Print function')
+print('The function is declared like this:')
+print('print("What to print")')
+
+print('Hello world!\nHello world!')
+
+print('Hello' + ' Tom!')
+
+name = input('What is your name?\n')
+print(f'Hello {name}!\nYour name is {len(name)} charaters long.')
